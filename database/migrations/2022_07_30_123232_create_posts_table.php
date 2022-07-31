de5291d73281f6b429aeb9d5a6ee51ec9262f906
newyee <takatelin01@icloud.com>
2022-07-31 15:02:17 +0900

    課題2の実装

---
+++ database/migrations/2022_07_30_123232_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->comment('投稿者のID');
+            $table->string('title')->comment('投稿のタイトル');
+            $table->string('content')->comment('投稿内容');
+            $table->integer('like')->nullable()->comment('いいね数');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
